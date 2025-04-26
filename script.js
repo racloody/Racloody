5f7b6780be8ae1dea8f4a5f4bc1afc685b2385a8
Racloody <gageotelouan@pm.me>
2025-04-26 13:25:21 +0200

    repair carousel

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const burgerMenu = document.getElementById('burger-menu');
-    const navLinks = document.querySelectorAll('nav ul');
-
-    burgerMenu.addEventListener('click', () => {
-        burgerMenu.classList.toggle('active'); // Toggle active class on burger menu
-        navLinks.forEach(nav => nav.classList.toggle('active'));
-    });
-});
